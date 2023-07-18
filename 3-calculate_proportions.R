2fc3b16b80d324102f1f1c38db49a83f5780f8f4
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-07-17 23:07:29 -0400

    feat: update the proportion counts

--- 3-calculate_proportions.R
+++ 3-calculate_proportions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8e73c81922186bbf13723f3e9bd6236c71d6a151f7688016f9e5ce5399ef704
-size 4140
+oid sha256:6e2323ca52698dbdc6bcbab388f5ec2905f529a398f1721afe40d5fe88385e8a
+size 4199
