c84d3f46c7ba8a899dfad507fa34bf97521cda4a
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-07-17 23:37:03 -0400

    fix: ensure valuenum column is present

--- 1-prepare_mimic_iv_data.R
+++ 1-prepare_mimic_iv_data.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d80ca94e3ca521b9a004a1b52beade6cef4fb8f93b0570912ba5b0f453db004
-size 1714
+oid sha256:0319d8dba958a0fe49d1e957440f0882e34a346996c7831d7746ca8c69cf64af
+size 1739
