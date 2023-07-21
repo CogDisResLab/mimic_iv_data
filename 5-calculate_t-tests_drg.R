7593f49f2f8179bed949cf9d1aa8d9c7b3751813
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-07-20 21:16:45 -0400

    fix: ensure ncases and nmatched contain numbers

--- 5-calculate_t-tests_drg.R
+++ 5-calculate_t-tests_drg.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9fee05c99708099e861dd7aa5f4e047282bf28e1e5da483e1f1efe6f1cc7d1d
-size 2297
+oid sha256:5845a9bd368bcdd9eae12bbf1dbfacbd0b6602cc015d12d18d97b01341b7d534
+size 2791
