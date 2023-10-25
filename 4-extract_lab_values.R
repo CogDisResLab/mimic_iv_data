e6b5028871cda9bd3f5de1a7503f613c8d27d79b
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-10-24 22:18:04 -0400

    feat: update labtest values

--- 4-extract_lab_values.R
+++ 4-extract_lab_values.R
@@ -12,9 +12,10 @@ extract_lab_values <- function(disease, labevents_data, labtest_list) {
     read_csv(file.path(in_dir, str_glue("matched_patients_{disease}.csv")))
 
   filtered_labevents <- labevents_data |>
+    select(-valueuom) |>
     inner_join(patient_list, by = "subject_id") |>
     inner_join(labtest_list, by = "itemid") |>
-    mutate(label = str_c(label, fluid, sep = " | ")) |>
+    mutate(label = str_c(label, fluid, valueuom, sep = " | ")) |>
     select(subject_id,
            gender,
            disease,
@@ -38,5 +39,10 @@ diseases <- list.files("ancillary/icd_codes/") |>
   keep(~ str_detect(.x, c("diabetes"), negate = TRUE))
 
 
-diseases |>
-  walk(~ extract_lab_values(.x, labevents_data, labtest_list))
+extract_lab_values(diseases[1], labevents_data, labtest_list)
+extract_lab_values(diseases[2], labevents_data, labtest_list)
+extract_lab_values(diseases[3], labevents_data, labtest_list)
+extract_lab_values(diseases[4], labevents_data, labtest_list)
+extract_lab_values(diseases[5], labevents_data, labtest_list)
+extract_lab_values(diseases[6], labevents_data, labtest_list)
+extract_lab_values(diseases[7], labevents_data, labtest_list)
