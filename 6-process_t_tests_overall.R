2d0eae039d26ec521d89d3a4c25a7e443023410c
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-13 15:06:00 -0500

    feat: add drg filtered results overall

--- 6-process_t_tests_overall.R
+++ 6-process_t_tests_overall.R
@@ -2,33 +2,43 @@
 
 library(tidyverse)
 
-t_test_results <- read_csv("results/compared_lab_values_overall_alzheimers.csv") |>
-  select(-ncases, -nmatched)
+process_t_test_results <- function(disease) {
 
-summary_data <- read_csv("results/summarised_lab_values_overall_alzheimers.csv") |>
-  mutate(disease_group = if_else(disease == 1, "CASE", "CTRL")) |>
-  select(-disease, -valueuom)
+  t_test_file <- str_glue("results/compared_lab_values_overall_{disease}_drg.csv")
+  summary_file <- str_glue("results/summarised_lab_values_overall_{disease}_drg.csv")
+
+  t_test_results <- read_csv(t_test_file)
+
+summary_data <- read_csv(summary_file) |>
+    mutate(disease_group = if_else(disease == 1, "CASE", "CTRL")) |>
+    select(-disease)
 
 
 significant_t_tests <- t_test_results |>
-  filter(significant) |>
-  select(label, estimate, p.value) |>
-  arrange(p.value) |>
-  mutate(
+    filter(significant) |>
+    select(label, cluster, estimate, p.value) |>
+    arrange(p.value) |>
+    mutate(
     label = fct_reorder(label, p.value),
     label = fct_rev(label)
   ) |>
-  left_join(summary_data, by = "label") |>
-  write_csv("results/compared_lab_values_overall_significant.csv")
+  left_join(summary_data, by = c("label", "cluster")) |>
+  write_csv(str_glue("results/compared_lab_values_overall_{disease}_drg_significant.csv"))
 
 significant_effect_sizes <- t_test_results |>
-  filter(significant) |>
-  select(label, effect_size, p.value) |>
-  arrange(p.value) |>
-  mutate(
+    filter(significant) |>
+    select(label, effect_size, cluster, p.value) |>
+    arrange(p.value) |>
+    mutate(
     label = fct_reorder(label, p.value),
     label = fct_rev(label)
   ) |>
-  left_join(summary_data, by = "label") |>
-  write_csv("results/compared_lab_values_overall_significant.csv")
+  left_join(summary_data, by = c("label", "cluster")) |>
+  write_csv(str_glue("results/compared_lab_values_overall_{disease}_drg_significant_effectsize.csv"))
+}
 
+
+diseases <- list.files("ancillary/icd_codes/") |>
+  keep(~ str_detect(.x, c("diabetes"), negate = TRUE))
+
+walk(diseases, process_t_test_results)
